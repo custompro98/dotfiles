0f8d18df9a2edc9b13ee43a63928a654e3ef3519
custompro98 <mitchjoa@gmail.com>
2019-11-27 17:59:33 -0500

    rm sublime settings

--- Sublime/Packages/ElasticsearchClient/commands/settings_show_active_server.py
+++
@@ -1,9 +0,0 @@
-from .base import SettingsBaseCommand
-
-
-class SettingsShowActiveServerCommand(SettingsBaseCommand):
-    command_name = "elasticsearch:settings-show-active-server"
-
-    def run(self):
-        self.show_object_output_panel(self.settings.active_server)
-        self.track_command()
