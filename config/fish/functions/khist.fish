38c2c672002f42ee607a1d101e6e34cdb4537f0a
custompro98 <mitchjoa@gmail.com>
2020-11-17 12:04:34 -0500

    Add khist

--- config/fish/functions/khist.fish
+++ config/fish/functions/khist.fish
@@ -1,10 +1,3 @@
-function khist -d 'View the history for a kube deployment' -a deployment rev
-  # this isn't working yet
-  # still getting "unknown flag "revision"
-  set -l revision (if set -q rev; echo -n "--revision $rev"; end)
-
-  echo $deployment
-  echo $revision
-
-  kubectl rollout history deployment/$deployment $revision
+function khist -d 'View the history for a kube deployment' -a deployment revision
+  kubectl rollout history deployment/$deployment (if [ -n "$revision" ]; echo "--revision=$revision"; end)
 end
