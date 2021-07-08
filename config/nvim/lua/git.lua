8cd5c72d28c37d51d92b44641eff6808fa688430
Mitch Joa <mitchjoa@stockx.com>
2021-07-08 08:50:45 -0400

    Bring back fugitive

--- config/nvim/lua/git.lua
+++ config/nvim/lua/git.lua
@@ -4,7 +4,5 @@ local api = vim.api
 
 -- ** Git ** --
 
-require'vgit'.setup()
-
-api.nvim_set_keymap('n', '<Leader>gb', ':VGit show_blame<CR>', {})
-api.nvim_set_keymap('n', '<Leader>gd', ':VGit buffer_preview<CR>', {})
+api.nvim_set_keymap('n', '<Leader>gb', ':Git blame<CR>', {})
+api.nvim_set_keymap('n', '<Leader>gd', ':Git diff %<CR>', {})
