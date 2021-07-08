7e01e286f26d73efe0b11722d7f349a9381ed5ab
custompro98 <mitchjoa@gmail.com>
2021-07-08 08:50:45 -0400

    Bring back fugitive

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -41,10 +41,7 @@ return packer.startup(function()
   -- development
   use 'b3nj5m1n/kommentary'
   use 'windwp/nvim-autopairs'
-  use {
-    'tanvirtin/vgit.nvim',
-    requires = 'nvim-lua/plenary.nvim'
-  }
+  use 'tpope/vim-fugitive'
 
   -- searching
   use 'vijaymarupudi/nvim-fzf'
