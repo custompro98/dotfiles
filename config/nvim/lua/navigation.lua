87ed8e96a2d8a34ce7d49f62179056ecc2fc5e54
custompro98 <mitchjoa@gmail.com>
2021-07-27 12:07:27 -0400

    Fix lsp document symbols

--- config/nvim/lua/navigation.lua
+++ config/nvim/lua/navigation.lua
@@ -25,7 +25,7 @@ api.nvim_set_keymap('n', '<Leader>o', '<cmd>lua require"fzf-commands".rg()<CR>',
 api.nvim_set_keymap('n', '<Leader>ff', '<cmd>lua require"telescope.builtin".find_files()<CR>', { noremap = true })
 api.nvim_set_keymap('n', '<Leader>fg', '<cmd>lua require"fzf-commands".rg()<CR>', { noremap = true })
 api.nvim_set_keymap('n', '<Leader>fr', '<cmd>lua require"telescope.builtin".lsp_references()<CR>', { noremap = true })
-api.nvim_set_keymap('n', '<Leader>fs', '<cmd>lua require"telescope.builtin".lsp_lsp_document_symbols()<CR>', { noremap = true })
+api.nvim_set_keymap('n', '<Leader>fs', '<cmd>lua require"telescope.builtin".lsp_document_symbols()<CR>', { noremap = true })
 
 -- split navigation
 api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true })
