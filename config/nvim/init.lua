ff7906c53ecc88e630ab7b64d8f6337450be9559
custompro98 <mitchjoa@gmail.com>
2021-10-06 15:37:55 -0400

    Move text manipulation into nvim configs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,3 +13,6 @@ require('git')
 require('navigation')
 require('terminal')
 require('treesitter')
+
+-- I can't convert this natively yet
+vim.cmd('source ~/.config/nvim/lua/text-manipulation.vim')
