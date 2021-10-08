be54c2e977e92a0ab25cd60ddcea6cb21004466d
custompro98 <mitchjoa@gmail.com>
2021-10-08 14:06:49 -0400

    Add output text to install script

--- install.sh
+++ install.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
+function print_checking() {
+  echo -e "\033[0;31m...checking for $1"
+}
+
+function print_installing() {
+  echo -e "\033[0;33m...installing $1"
+}
+
+function print_installed() {
+  echo -e "\033[0;32m... $1 installed"
+}
+
+print_checking "brew"
 if ! command -v brew &> /dev/null
 then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  print_installing "brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+print_installed "brew"
+
+
+print_installing "powerline fonts"
 git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
 cd .. && rm -rf fonts
+print_installed "powerline fonts"
 
+print_installing "applications from brew"
 brew install iterm2 neovim tree bat chrome-cli autojump fzf fd ripgrep
+print_installed "applications from brew"
 
-if [[ -n "$(command -v pip3)" ]]; then
+print_checking "pynvim"
+if command -v pip3
+then
+  print_installing "pynvim"
   pip3 install pynvim
 fi
 
-if [[ -n "$(command -v pip2)" ]]; then
+if command -v pip2
+then
+  print_installing "pynvim"
   pip2 install pynvim
 fi
+print_installed "pynvim"
 
+print_installing "dotfiles"
 ln -sfn ~/.dotfiles/config/nvim/ ~/.config/nvim
 ln -sfn ~/.dotfiles/rgignore ~/.rgignore
 ln -sfn ~/.dotfiles/bash_profile ~/.bash_profile
@@ -28,13 +56,31 @@ ln -sfn ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sfn ~/.dotfiles/gitignore ~/.gitignore
 ln -sfn ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sfn ~/.dotfiles/scripts/templatemux /usr/local/bin/templatemux
+print_installed "dotfiles"
 
 # Install NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
-nvm install --lts
-nvm use --lts
+print_checking "nvm"
+if ! command -v nvm &> /dev/null
+then
+  print_installing "nvm"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
+  nvm install --lts
+  nvm use --lts
+fi
+print_installed "nvm"
 
-npm install -g prettier
+print_checking "prettier"
+if ! command -v prettier &> /dev/null
+then
+  print_installing "prettier"
+  npm install -g prettier
+fi
+print_installed "prettier"
+
+print_installing "nvim plugins"
 nvim +PackerInstall +qall
+print_installed "nvim plugins"
 
+print_installing "fish"
 ./install-fish.sh
+print_installed "fish"
