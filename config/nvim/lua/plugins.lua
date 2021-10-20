330beadd389228b0c73050b89a424dcaffc64e79
custompro98 <mitchjoa@gmail.com>
2021-10-20 14:46:25 -0400

    Add playground for treesitter

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -37,6 +37,7 @@ return packer.startup(function(use)
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
+  use 'nvim-treesitter/playground'
 
   -- development
   use 'b3nj5m1n/kommentary'
