92795dff4d6711ee85843c6e85b203e557e527c7
custompro98 <mitchjoa@gmail.com>
2021-10-21 09:28:02 -0400

    Add query linter

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -13,6 +13,11 @@ require'nvim-treesitter.configs'.setup {
     enable = false,
     disable = {},
   },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = { "BufWrite", "CursorHold" }
+  },
   ensure_installed = 'maintained',
   autopairs = { enable = true }
 }
