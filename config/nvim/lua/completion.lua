e7073edf056002ddcc695942b911c5f12dbd831b
custompro98 <mitchjoa@gmail.com>
2021-11-01 14:54:06 -0400

    Remove deprecated cmp setup in autopairs

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -21,7 +21,7 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- disabled in favor of nvim-autopairs
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- disabled in favor of nvim-autopairs
   },
   sources = cmp.config.sources(
     {
@@ -51,17 +51,5 @@ require('nvim-autopairs').setup({
     lua = {'string'},-- it will not add pair on that treesitter node
     javascript = {'template_string'},
     java = false,-- don't check treesitter on java
-  }
-})
-
--- you need setup cmp first put this after cmp.setup()
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
   }
 })
