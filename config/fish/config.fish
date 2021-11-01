79c9ee09b929a0ec3994462fc0607d9820cae1e2
custompro98 <mitchjoa@gmail.com>
2021-11-01 16:03:11 -0400

    Install node if not present

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,8 +14,12 @@ set -x FZF_LEGACY_KEYBINDINGS 0
 # sourcing shell tools
 ## nvm
 ### nvm is acting funky and resourcing itself a bunch, just use it when you need it
-set NVM_CUR_VSN (nvm current)
-[ -z "$NVM_CUR_VSN" ]; and nvm use lts/fermium
+set NODE_VERSION "lts/fermium"
+set NVM_INSTALLED (nvm list $NODE_VERSION)
+[ -z "$NVM_INSTALLED" ]; and nvm install $NODE_VERSION
+
+set NVM_VSN_SET (nvm current)
+[ -z "$NVM_VSN_SET" ]; and nvm use $NODE_VERSION
 
 ## g
 set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
