d1a6577c4f665403dc8b79f9f2bdbcb63133b0a9
custompro98 <mitchjoa@gmail.com>
2022-07-26 15:01:32 -0400

    Support newer macs for fish installation

--- install-fish.sh
+++ install-fish.sh
@@ -5,11 +5,12 @@ brew install fish
 ln -sfn ~/.dotfiles/config/fish/ ~/.config/fish
 
 # install fisher for plugin support
-fish -c '[ ! -f ~/.dotfiles/config/fish/fish_plugins ]; and curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
-fish -c 'fisher update'
-fish -c '_nvm_install' # make sure nvm is configured
+/opt/homebrew/bin/fish -c '[ ! -f ~/.dotfiles/config/fish/fish_plugins ]; and curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
+/opt/homebrew/bin/fish -c 'fisher update'
+/opt/homebrew/bin/fish -c '_nvm_install' # make sure nvm is configured
 
 # add fish to the allowed list of shells
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
+echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 # set fish as the default shell
-chsh -s /usr/local/bin/fish
+chsh -s /opt/homebrew/bin/fish
+/opt/homebrew/bin/fish -c 'set -U fish_user_paths /opt/homebrew/bin $fish_user_paths'
