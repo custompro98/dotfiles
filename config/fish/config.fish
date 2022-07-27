f3f6e4b087886c990f7af2ca14ed2d58e9648abd
custompro98 <mitchjoa@gmail.com>
2022-07-27 09:54:49 -0400

    Configure autojump for fish with new homebrew path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -26,7 +26,7 @@ if status --is-interactive
   set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
 
   ## autojump
-  [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+  [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
 
   # source company-specific aliases
   [ -f ~/.company.fish ]; and source ~/.company.fish
