e367f03b242bd23a9e5f14bf3ea42e1f04fc1d80
custompro98 <mitchjoa@gmail.com>
2022-08-30 17:20:12 -0400

    Add support for ftplugin overrides

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,10 @@
 -- leader
 vim.g.mapleader = ';'
 
+-- set runtime
+local override_path = "~/.config/nvim/override/after"
+vim.opt.runtimepath:append(override_path)
+
 require('plugins')
 
 require('_lsp')
