39c94d9100d3f04189485c9de019c967835dd5b7
custompro98 <mitchjoa@gmail.com>
2022-09-01 13:20:19 -0400

    Show full filepath in lualine

--- config/nvim/lua/appearance.lua
+++ config/nvim/lua/appearance.lua
@@ -36,7 +36,7 @@ require'lualine'.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_c = { {'filename', path=1} },
     lualine_x = {'encoding', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
