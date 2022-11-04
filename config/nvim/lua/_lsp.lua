2d3df8473d26e1c4580801ead5ab7fdf59f1c30d
custompro98 <mitchjoa@gmail.com>
2022-11-03 21:01:50 -0400

    update nvim cmp deprecated field

--- config/nvim/lua/_lsp.lua
+++ config/nvim/lua/_lsp.lua
@@ -87,7 +87,7 @@ local lsp_flags = {
 }
 
 local nvim_lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 for _, lsp in pairs(lsps) do
   local opts = {
