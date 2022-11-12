ecd0a9a928e7cc9eff3530ac2d282c0133ac3450
custompro98 <mitchjoa@gmail.com>
2022-11-12 10:48:50 -0500

    fix bash conditional in node2 intaller

--- install-node2.sh
+++ install-node2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [! -d "~/dev/microsoft/vscode-node-debug2"] then
+if [! -d "~/dev/microsoft/vscode-node-debug2"]; then
   mkdir -p ~/dev/microsoft
   git clone https://github.com/microsoft/vscode-node-debug2.git ~/dev/microsoft/vscode-node-debug2
 fi
