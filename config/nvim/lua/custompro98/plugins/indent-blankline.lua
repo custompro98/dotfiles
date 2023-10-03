5bef4098d6e5ead6f8a5438f25e298b40bba341d
custompro98 <mitchjoa@gmail.com>
2023-10-03 09:49:20 -0400

    Update indent blankline to v3

--- config/nvim/lua/custompro98/plugins/indent-blankline.lua
+++ config/nvim/lua/custompro98/plugins/indent-blankline.lua
@@ -2,9 +2,11 @@ return {
     {
         -- Add indentation guides even on blank lines
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
         opts = {
-            char = "┊",
-            show_trailing_blankline_indent = false,
+            indent = {
+              char = "┊",
+            }
         },
     },
 }
