454faf0503d3af10f0e3fe95afec859a7bd639c8
custompro98 <mitchjoa@gmail.com>
2024-01-11 09:39:44 -0500

    chore: harpoon2

--- config/nvim/lua/custompro98/plugins/harpoon.lua
+++ config/nvim/lua/custompro98/plugins/harpoon.lua
@@ -1,15 +1,41 @@
 return {
-    {
-        "ThePrimeagen/harpoon",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            vim.keymap.set('n', '<Leader>ha', require('harpoon.mark').add_file, { noremap = true })
-            vim.keymap.set('n', '<Leader>hu', require('harpoon.ui').toggle_quick_menu, { noremap = true })
-            vim.keymap.set('n', '<Leader>a', function() require('harpoon.ui').nav_file(1) end, { noremap = true })
-            vim.keymap.set('n', '<Leader>s', function() require('harpoon.ui').nav_file(2) end, { noremap = true })
-            vim.keymap.set('n', '<Leader>d', function() require('harpoon.ui').nav_file(3) end, { noremap = true })
-        end
-    },
+	{
+		"ThePrimeagen/harpoon",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		branch = "harpoon2",
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+
+			vim.keymap.set("n", "<Leader>ha", function()
+				harpoon:list():append()
+			end)
+
+			vim.keymap.set("n", "<Leader>hu", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<Leader>a", function()
+				harpoon:list():select(1)
+			end)
+
+			vim.keymap.set("n", "<Leader>s", function()
+				harpoon:list():select(2)
+			end)
+
+			vim.keymap.set("n", "<Leader>d", function()
+				harpoon:list():select(3)
+			end)
+
+			vim.keymap.set("n", "<leader>p", function()
+				harpoon:list():prev()
+			end)
+
+			vim.keymap.set("n", "<Leader>n", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
 }
