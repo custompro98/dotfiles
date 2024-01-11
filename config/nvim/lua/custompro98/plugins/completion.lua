031b7249cd36d1c668dd40e98b089dce02dda9ec
custompro98 <mitchjoa@gmail.com>
2024-01-11 09:19:44 -0500

    feat: re-enable completion

--- config/nvim/lua/custompro98/plugins/completion.lua
+++ config/nvim/lua/custompro98/plugins/completion.lua
@@ -1,23 +1,67 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            -- LSP Autocomplete
-            "hrsh7th/cmp-nvim-lsp",
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			-- LSP Autocomplete
+			"hrsh7th/cmp-nvim-lsp",
 
-            -- Snippet Autocomplete
-            {
-                "saadparwaiz1/cmp_luasnip",
-                dependencies = {
-                    "L3MON4D3/LuaSnip",
-                    "rafamadriz/friendly-snippets",
-                },
-            },
+			-- Snippet Autocomplete
+			{
+				"saadparwaiz1/cmp_luasnip",
+				dependencies = {
+					"L3MON4D3/LuaSnip",
+					"rafamadriz/friendly-snippets",
+				},
+			},
 
-            -- Generic Autocomplete
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-nvim-lua",
-            "custompro98/cmp-kitty",
-        },
-    },
+			-- Generic Autocomplete
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lua",
+			"custompro98/cmp-kitty",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end),
+					["<C-p>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = {
+					{ name = "kity" },
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 }
