86b2345884a11f2057e43ae1ad1c5f33e25775b3
custompro98 <mitchjoa@gmail.com>
2024-12-16 10:05:39 -0500

    chore: Use nvim commands for term

--- config/nvim/lua/custompro98/terminal.lua
+++ config/nvim/lua/custompro98/terminal.lua
@@ -1,14 +1,23 @@
 -- ** Terminal ** --
 
 -- enter terminal mode
-vim.keymap.set("n", "<Leader>t", function ()
-    OpenWindow("botright", vim.api.nvim_list_uis()[1].height/5, "split|terminal")
+vim.keymap.set("n", "<Leader>t", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("L")
+	vim.api.nvim_win_set_width(0, 120)
+	-- vim.api.nvim_open_term(0, { force_crlf = true })
 end, { noremap = true })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	group = vim.api.nvim_create_augroup("OnTermOpen", { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+		vim.cmd.startinsert()
+	end,
+})
 
 -- leave terminal mode
 vim.keymap.set("t", "<Leader>t", "<C-\\><C-n>", {})
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { remap = true })
-
-function OpenWindow(position, height, cmd)
-    vim.cmd(position..height..cmd)
-end
