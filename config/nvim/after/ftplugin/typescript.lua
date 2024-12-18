a012dd12aace099ed85f85985fdbc98df8a3e331
custompro98 <mitchjoa@gmail.com>
2024-12-18 13:45:28 -0500

    feat: typescript shortcuts

--- config/nvim/after/ftplugin/typescript.lua
+++ config/nvim/after/ftplugin/typescript.lua
@@ -12,3 +12,15 @@ end
 vim.keymap.set("n", "<leader>yb", function()
 	vim.fn.chansend(get_term_id(), "yarn build\n")
 end, { remap = true })
+
+vim.keymap.set("n", "<leader>yw", function()
+	vim.fn.chansend(get_term_id(), "yarn watch\n")
+end, { remap = true })
+
+vim.keymap.set("n", "<leader>yt", function()
+	vim.fn.chansend(get_term_id(), "yarn test\n")
+end, { remap = true })
+
+vim.keymap.set("n", "<leader>ytw", function()
+	vim.fn.chansend(get_term_id(), "yarn test:watch\n")
+end, { remap = true })
