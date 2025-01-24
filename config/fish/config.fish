1d7ebab3ca0b0ac30aeb5c087d32477f953eb28b
custompro98 <mitchjoa@gmail.com>
2025-01-24 09:38:03 -0500

    fix: NVM auto use

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,7 +21,8 @@ if status --is-interactive
 
   # sourcing shell tools
   ## nvm
-  set --universal nvm_default_version 20
+  set --universal nvm_default_version lts
+  __check_nvm
 
   ## g
   set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
