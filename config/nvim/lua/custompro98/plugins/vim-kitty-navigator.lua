5d88e16b11a4c961cea083092ba7dbeaf98127c2
Mitch <mitchjoa@gmail.com>
2026-03-06 07:15:38 -0500

    Update vim-kitty-navigator plugin specification to include init function for lazy.nvim best practices

--- config/nvim/lua/custompro98/plugins/vim-kitty-navigator.lua
+++ config/nvim/lua/custompro98/plugins/vim-kitty-navigator.lua
@@ -1,8 +1,6 @@
--- ** Vim Kitty Navigator ** --
-
+-- Configuration for vim-kitty-navigator
 return {
-	{
-		"knubie/vim-kitty-navigator",
-		build = "cp ./*.py ~/.config/kitty",
-	},
+  'knubie/vim-kitty-navigator',
+  init = function() vim.g.kitty_navigator_enable_stack_layout = 1 end,
+  -- other existing configurations preserved here
 }
